6e6b6b72683d93102c18a8328551f94f46466685
Becca Wyland <rwyland@uwalumni.com>
2021-07-20 13:33:09 -0500

    start the fetch event listener for service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -45,3 +45,10 @@ self.addEventListener('activate', function (e) {
         })
     )
 })
+
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        
+    )
+})
