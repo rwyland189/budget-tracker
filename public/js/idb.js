d0f268d352d02120ab2cfb1b41f5971db3e5095b
Becca Wyland <rwyland@uwalumni.com>
2021-07-20 12:33:25 -0500

    write updateBudget() and write getAll code

--- public/js/idb.js
+++ public/js/idb.js
@@ -41,3 +41,48 @@ function saveRecord(record) {
     // add record to your store with add method
     budgetObjectStore.add(record);
 }
+
+function updateBudget() {
+    // open a transaction on your db
+    const transaction = db.transaction(['budget_update'], 'readwrite');
+
+    // access your object store
+    const budgetObjectStore = transaction.objectStore('budget_udpate');
+
+    // get all records from store and set to a variable
+    const getAll = budgetObjectStore.getAll();
+
+    // upon a successful .getAll() execution, run this function
+    getAll.onsuccess = function() {
+        // if there was data in indexedDb's store, let's send it to the api server
+        if (getAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+                .then(response => response.json())
+                .then(serverResponse => {
+                    if (serverResponse.message) {
+                        throw new Error(serverResponse);
+                    }
+                    // open one more transaction
+                    const transaction = db.transaction(['budget_update'], 'readwrite');
+
+                    // access the budget_update object store
+                    const budgetObjectStore = transaction.objectStore('budget_update');
+
+                    // clear all items in your store
+                    budgetObjectStore.clear();
+
+                    alert('All budget updates have been submitted!');
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+        }
+    };
+}
